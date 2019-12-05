dcc16d1217611b246b287460f528e55659520455
AgrawalTrisha <agrawaltrisha@gmail.com>
2019-12-04 21:54:41 -0600

    Cleaned up code

--- AdventOfCode/src/AdventOfCode.java
+++ AdventOfCode/src/AdventOfCode.java
@@ -2,8 +2,7 @@ public class AdventOfCode {
     // Day 1
     // Problem 1
     public static int d1p1(int mass) {
-        int fuel = mass / 3 - 2;
-        return fuel;
+        return mass / 3 - 2;
     }
     // Problem 2
     public static int d1p2(int mass) {
